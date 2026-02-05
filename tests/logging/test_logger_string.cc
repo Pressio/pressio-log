80457120fc81a183e2121a452bdafedae633f6b1
Chad Malmrose <chadmalmrose1@gmail.com>
2026-02-05 09:22:33 -0500

    #20: Logger string unit tests use same level, added new expect to ensure good output

--- tests/logging/test_logger_string.cc
+++ tests/logging/test_logger_string.cc
@@ -4,22 +4,23 @@
 #include "LoggerTest.hpp"
 #include "pressio-log/core.hpp"
 
-TEST_F(LoggerTest, Logger_String_Templated) {
+TEST_F(LoggerTest, Logger_String_Formatting) {
     PRESSIOLOG_SET_LEVEL(pressiolog::LogLevel::debug);
 
     CoutRedirector redirect;
 
     PRESSIOLOG_SPARSE("{}", "hey there");
-    PRESSIOLOG_INFO("{} {}", "two", "strings");
-    PRESSIOLOG_DEBUG("{}", "Debug");
-    PRESSIOLOG_WARNING("{}: {}", 2, "number");
-    PRESSIOLOG_ERROR("{}", "Error");
+    PRESSIOLOG_SPARSE("{} {}", "two", "strings");
+    PRESSIOLOG_SPARSE("{}", "Debug");
+    PRESSIOLOG_SPARSE("{}: {}", 2.1, "number");
+    PRESSIOLOG_SPARSE("{}", "Error");
 
     std::string output = redirect.str();
 
     EXPECT_TRUE(check_output(output, "hey there",   true));
     EXPECT_TRUE(check_output(output, "two strings", true));
     EXPECT_TRUE(check_output(output, "Debug",       true));
-    EXPECT_TRUE(check_output(output, "2: number",   true));
+    EXPECT_TRUE(check_output(output, "2.1: number",   true));
     EXPECT_TRUE(check_output(output, "Error",       true));
+    EXPECT_TRUE(check_output(output, "could not format given string",   false));
 }
